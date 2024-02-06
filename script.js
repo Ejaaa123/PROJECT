c54002643cb7badc49ad17453fe4ec38e5ebf8f1
Ejaaa123 <150584470+Ejaaa123@users.noreply.github.com>
2024-02-06 14:30:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.querySelector("#button");
+let sidebar = document.querySelector(".sidebar");
+
+
+
+
+button.onclick = function (){
+    sidebar.classList.toggle("active");
+};
